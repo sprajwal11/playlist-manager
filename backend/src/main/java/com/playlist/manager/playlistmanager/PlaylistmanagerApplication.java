cce8b5fd2a45c2a7c5de06dbe4d0803416919010
Prajwal <sharmaprajwal11@gmail.com>
2023-12-29 15:04:08 +0530

    initial commit

---
+++ backend/src/main/java/com/playlist/manager/playlistmanager/PlaylistmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.playlist.manager.playlistmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/playlist/manager/playlistmanager/PlaylistmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playlist.manager.playlistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
